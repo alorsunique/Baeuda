2484291fa317bae9cee2e0f0db6497bef0c56527
alorsunique <sounique7800@gmail.com>
2024-03-27 20:43:31 +0800

    2024 March 27

--- main.py
+++ main.py
@@ -1,1 +1,8 @@
 print("Hello World")
+
+import tensorflow as tf
+
+print(tf.__version__)
+print("Num GPUs Available: ", len(tf.config.list_physical_devices('GPU')))
+
+print(tf.reduce_sum(tf.random.normal([1000, 1000])))
